4e52665a58efbefebc66e4f965b0b987e421bcc8
Gabriel Gonzales <gabrielgonzales0007@gmail.com>
2025-03-14 20:29:49 -0300

    Inicializando váriaveis e constantes no javascript

---
+++ main.js
@@ -0,0 +1,8 @@
+// Simulação de adição de produtos ao carrinho.
+
+const botoesAdicionar = document.querySelectorAll('.adicionar');
+const listaPedido     = document.getElementById('lista-pedido');
+const totalCarrinho = document.getElementById('total');
+
+let total = 0;
+
