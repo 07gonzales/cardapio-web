d541270f9cde48fe8393050a6f53db70016111a0
Gabriel Gonzales <gabrielgonzales0007@gmail.com>
2025-03-14 21:48:27 -0300

    Criando funções para armazenar os valores e nomes dos produtos em uma váriavel e exibindo ela no index.html

--- main.js
+++ main.js
@@ -6,3 +6,27 @@ const totalCarrinho = document.getElementById('total');
 
 let total = 0;
 
+botoesAdicionar.forEach(botao => {
+    botao.addEventListener('click', () => {
+        const produto = botao.parentElement;
+        const nome = produto.querySelector('h3').textContent;
+        const preco = parseFloat(produto.querySelector('.preco').textContent.replace('R$', ''));
+
+        const itemPedido = document.createElement('li');
+        itemPedido.textContent = `${nome} = R$ ${preco.toFixed(2)}`;
+
+        listaPedido.appendChild(itemPedido);
+
+        total += preco;
+
+
+        totalCarrinho.textContent = `Total: R$ ${total.toFixed(2)}`;
+
+    });
+});
+
+const botaoFinalizarPedido = document.getElementById('finalizar-pedido');
+
+botaoFinalizarPedido.addEventListener('click', () => {
+    alert("Pedido realizado com sucesso (simulação).");
+});
